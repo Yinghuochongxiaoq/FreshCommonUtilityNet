10c33190909d8ab14428d5bc7420fe26518bc67d
FreshMan <qinbocai@sina.cn>
2017-08-30 10:01:20 +0800

    Delete unuse namespace.

--- FreshCommonUtilityNetTest/Program.cs
+++ FreshCommonUtilityNetTest/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
-using System.Text;
-using FreshCommonUtility.RegExp;
 using FreshCommonUtilityNetTest.Dapper;
 using FreshCommonUtilityNetTest.DataConvert;
 using FreshCommonUtilityNetTest.DeepCopy;
